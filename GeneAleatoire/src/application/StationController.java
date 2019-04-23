0712b969ef4f07a396958f5ea19851e5b6bed0c8
Florent <Florent@LAPTOP-AB99RUP6>
2019-04-23 23:25:17 +0200

    bqeagqHERJY

--- GeneAleatoire/src/application/StationController.java
+++ GeneAleatoire/src/application/StationController.java
@@ -1,5 +1,6 @@
 package application;
 
+import metier.GenerationLois;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -93,7 +94,7 @@ public class StationController {
 	/**
 	 * Réinitialise tous les champs
 	 */
-	public void reinitialiser(ActionEvent evt){
+	public void reinitialiser(){
 
 		// Vide les classes
 		classe1.setText(null);
@@ -218,8 +219,13 @@ public class StationController {
 	 * @param Evenement au clic du bouton "Loi Exponentielle"
 	 */
 	public void actionLoiExpo(ActionEvent evt){
+		
+		// Tout les cases sont vidées
+		reinitialiser();
+		
 		System.out.println("\nAction loi Exponentielle");
-		//initialisation des champs texte de classe
+		
+		// Initialisation des champs texte de classe
 		classe1.setText("0-0,05");
 		classe2.setText("0,05-0,1");
 		classe3.setText("0,1-0,2");
@@ -228,6 +234,19 @@ public class StationController {
 		classe6.setText("0,5-0,8");
 		classe7.setText("0,8-1,1");
 		classe8.setText("1,1-10");
+		
+		// Initialisation des champs texte de n_th
+		th1.setText("9");
+		th2.setText("8");
+		th3.setText("14");
+		th4.setText("12");
+		th5.setText("18");
+		th6.setText("16");
+		th7.setText("9");
+		th8.setText("12");
+		
+		// Génération des n_obs
+		
 	}
 
 	/**
