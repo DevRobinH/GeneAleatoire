338acaf1f1859fd379cc7eadbe07e429bd890e00
Florent <Florent@LAPTOP-AB99RUP6>
2019-04-23 17:49:46 +0200

    Début IHM

--- GeneAleatoire/src/application/Main.java
+++ GeneAleatoire/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -12,10 +13,19 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,600,500);
+			
+			// Titre de la fenetre
+			primaryStage.setTitle("Station Service Carburant");
+			
+			// Logo 3iL
+			primaryStage.getIcons().setAll(new Image(getClass().getResource("3ilogo.png").toExternalForm()));
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
